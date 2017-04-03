8f775118fdda1d89aec9b58db6be6c105489a596
vindarel <ehvince@mailz.org>
2017-04-03 19:06:58 +0200

    use Codex documentation generator

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:series)
+ :documents ((:title "Series"
+              :authors ("Richard C. Waters")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
